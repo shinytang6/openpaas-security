60adc934b3bd26e9043e1dbda5c85422dba4016e
shinytang6 <1074461480@qq.com>
2019-03-25 18:45:59 +0800

    update deployment

--- deployment/deployment.go
+++ deployment/deployment.go
@@ -1,68 +1,44 @@
 package deployment
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	apps_v1beta1 "k8s.io/api/apps/v1beta1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/util/yaml"
-	"k8s.io/client-go/kubernetes/typed/apps/v1beta1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/typed/apps/v1beta1"
 )
 
-func CreateDeployment(deploymentClient v1beta1.DeploymentInterface)  {
-	var (
-		deployYaml []byte
-		deployJson []byte
-		deployment = apps_v1beta1.Deployment{}
-		err error
-	)
-	if deployYaml, err = ioutil.ReadFile("./nginx.yaml"); err != nil {
-		panic(err.Error())
-	}
-
-	if deployJson, err = yaml.ToJSON(deployYaml); err != nil {
-		panic(err.Error())
-	}
-	// JSON转struct
-	if err = json.Unmarshal(deployJson, &deployment); err != nil {
-		panic(err.Error())
-	}
-
-	if _, err = GetDeployment(deploymentClient, deployment.Name); err != nil {
+func CreateDeployment(deploymentClient v1beta1.DeploymentInterface, deployment apps_v1beta1.Deployment)  {
+	if _, err := GetDeployment(deploymentClient, deployment.Name); err != nil {
 		if !errors.IsNotFound(err) {
 			panic(err.Error())
 			return
 		}
-		fmt.Println("Start creating deployment...")
+		fmt.Printf("Start creating deployment...\n")
 		result, err := deploymentClient.Create(&deployment)
 		if err != nil {
 			panic(err.Error())
 		}
 		fmt.Printf("Deployment %s created!\n", result.GetObjectMeta().GetName())
 	} else {
-		fmt.Printf("deployments.apps %s already exists\n", deployment.Name)
-		fmt.Println("Start updating deployment...")
 		UpdateDeployment(deploymentClient, deployment)
-		fmt.Printf("Deployment %s updated!\n", deployment.Name)
 	}
 }
 
 func GetDeployment(deploymentClient v1beta1.DeploymentInterface, deploymentName string) (*apps_v1beta1.Deployment, error) {
-	var (
-		deployment  = &apps_v1beta1.Deployment{}
-		err error
-	)
-	deployment, err = deploymentClient.Get(deploymentName, meta_v1.GetOptions{})
+	deployment, err := deploymentClient.Get(deploymentName, meta_v1.GetOptions{})
 	return deployment, err
 }
 
 func UpdateDeployment(deploymentClient v1beta1.DeploymentInterface, deployment apps_v1beta1.Deployment) (*apps_v1beta1.Deployment, error) {
-	var (
-		new_deployment  = &apps_v1beta1.Deployment{}
-		err error
-	)
-	new_deployment, err = deploymentClient.Update(&deployment)
+	fmt.Printf("Start updating deployment...\n")
+	new_deployment, err := deploymentClient.Update(&deployment)
+	fmt.Printf("Deployment %s updated!\n", deployment.Name)
 	return new_deployment, err
+}
+
+func DeleteDeployment(deploymentClient v1beta1.DeploymentInterface, deploymentName string) error {
+	fmt.Printf("Start deleting deployment %s...\n", deploymentName)
+	err := deploymentClient.Delete(deploymentName, &meta_v1.DeleteOptions{})
+	return err
 }
--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"github.com/shinytang6/openpaas-security/client"
-	"github.com/shinytang6/openpaas-security/deployment"
+	deploy "github.com/shinytang6/openpaas-security/deployment"
+	apps_v1beta1 "k8s.io/api/apps/v1beta1"
+	"k8s.io/apimachinery/pkg/util/yaml"
 )
 
 func main() {
+	var (
+		deployYaml []byte
+		deployJson []byte
+		deployment = apps_v1beta1.Deployment{}
+		err error
+	)
 	clientset := client.InitClient()
 	deploymentClient := clientset.AppsV1beta1().Deployments("default")
-	//fmt.Println(deployment)
-	//fmt.Println(deployment.Items[0].Spec.Template.Spec.Containers)
-	//fmt.Println(len(deployment.Items))
-	//fmt.Println(clientset)
-	deployment.CreateDeployment(deploymentClient)
+	if deployYaml, err = ioutil.ReadFile("./nginx.yaml"); err != nil {
+		panic(err.Error())
+	}
+
+	if deployJson, err = yaml.ToJSON(deployYaml); err != nil {
+		panic(err.Error())
+	}
+	// JSON转struct
+	if err = json.Unmarshal(deployJson, &deployment); err != nil {
+		panic(err.Error())
+	}
+
+	deploy.CreateDeployment(deploymentClient, deployment)
+	//deploy.DeleteDeployment(deploymentClient, deployment.Name)
 }
