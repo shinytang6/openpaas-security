87da82b079768a789d4bf00c8896de74e4ec5c4a
shinytang6 <1074461480@qq.com>
2019-06-16 15:49:47 +0800

    update db.sql

--- db.sql
+++ db.sql
@@ -54,14 +54,14 @@ DROP TABLE IF EXISTS `Student`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Student` (
   `userId` int(11) NOT NULL AUTO_INCREMENT,
-  `password` varchar(20) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
   `name` varchar(20) DEFAULT NULL,
   `studentId` varchar(20) DEFAULT NULL,
   `class` varchar(20) DEFAULT NULL,
   `email` varchar(20) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,7 +70,7 @@ CREATE TABLE `Student` (
 
 LOCK TABLES `Student` WRITE;
 /*!40000 ALTER TABLE `Student` DISABLE KEYS */;
-INSERT INTO `Student` VALUES (2,'******','student1','1','F1500001','1064257910@qq.com','18264128133'),(3,'******','student2','2','F1503005','1054264220@qq.com','13416455213'),(4,'******','student3','3','F1503002','1546452972@qq.com','13256482134'),(5,'******','student4','4','F1503006','694285431@qq.com','13462489521'),(6,'******','student5','5','F1502009','594612378@qq.com','13579462321');
+INSERT INTO `Student` VALUES (7,'e10adc3949ba59abbe56e057f20f883e','student1','515020910111','F1503554','1054642150@qq.com','18246123333'),(8,'e10adc3949ba59abbe56e057f20f883e','student2','516201348200','F1503002','12521364222@qq.com','15213465522'),(10,'e10adc3949ba59abbe56e057f20f883e','student3','542310201301','F1503446','829465555@qq.com','13541389543');
 /*!40000 ALTER TABLE `Student` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,13 +83,13 @@ DROP TABLE IF EXISTS `SystemAdmin`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `SystemAdmin` (
   `userId` int(11) NOT NULL AUTO_INCREMENT,
-  `password` varchar(20) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
   `name` varchar(20) DEFAULT NULL,
   `systemAdminId` varchar(20) DEFAULT NULL,
   `email` varchar(20) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +98,7 @@ CREATE TABLE `SystemAdmin` (
 
 LOCK TABLES `SystemAdmin` WRITE;
 /*!40000 ALTER TABLE `SystemAdmin` DISABLE KEYS */;
-INSERT INTO `SystemAdmin` VALUES (1,'******','sysAdmin','123','test@admin.com','18224645213');
+INSERT INTO `SystemAdmin` VALUES (2,'e10adc3949ba59abbe56e057f20f883e','sysAdmin','1','system@sjtu.edu.cn','12845213321');
 /*!40000 ALTER TABLE `SystemAdmin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -111,13 +111,13 @@ DROP TABLE IF EXISTS `Teacher`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Teacher` (
   `userId` int(11) NOT NULL AUTO_INCREMENT,
-  `password` varchar(20) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
   `name` varchar(20) DEFAULT NULL,
   `TeacherId` varchar(20) DEFAULT NULL,
   `email` varchar(20) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -126,7 +126,7 @@ CREATE TABLE `Teacher` (
 
 LOCK TABLES `Teacher` WRITE;
 /*!40000 ALTER TABLE `Teacher` DISABLE KEYS */;
-INSERT INTO `Teacher` VALUES (3,'******','teacher1','1','teacher1@sjtu.edu.cn','18224612354'),(4,'******','teacher2','2','teacher2@sjtu.edu.cn','14325641238'),(5,'******','teacher3','3','teacher3@sjtu.edu.cn','13469755542');
+INSERT INTO `Teacher` VALUES (8,'e10adc3949ba59abbe56e057f20f883e','teacher1','1','teacher1@sjtu.edu.cn','13494426282'),(9,'e10adc3949ba59abbe56e057f20f883e','teacher2','2','teacher2@sjtu.edu.cn','15462138544');
 /*!40000 ALTER TABLE `Teacher` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -139,13 +139,13 @@ DROP TABLE IF EXISTS `UserAdmin`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `UserAdmin` (
   `userId` int(11) NOT NULL AUTO_INCREMENT,
-  `password` varchar(20) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
   `name` varchar(20) DEFAULT NULL,
   `userAdminId` varchar(20) DEFAULT NULL,
   `email` varchar(20) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -154,7 +154,7 @@ CREATE TABLE `UserAdmin` (
 
 LOCK TABLES `UserAdmin` WRITE;
 /*!40000 ALTER TABLE `UserAdmin` DISABLE KEYS */;
-INSERT INTO `UserAdmin` VALUES (1,'csacs','Shiny','1','1162154239@qq.com','18224645213');
+INSERT INTO `UserAdmin` VALUES (1,'csacs','Shiny','1','1162154239@qq.com','18224645213'),(2,'e10adc3949ba59abbe56e057f20f883e','userAdmin','2','1542622452@qq.com','16423485222');
 /*!40000 ALTER TABLE `UserAdmin` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -167,4 +167,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-06-15 16:12:26
+-- Dump completed on 2019-06-16 15:49:03
